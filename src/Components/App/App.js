323f23964f4434b736c76b0bfd1bf9da21a484fb
Gleb <ch.cookiehunter@gmail.com>
2018-09-20 21:52:31 +0200

    code refactored

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,15 +5,26 @@ import Navbar from '../Navbar/Navbar';
 import DictionaryList from '../DictionaryList/DictionaryList';
 import Editor from '../Editor/Editor';
 import Footer from '../Footer/Footer';
+import Validator from '../../Validator/Validator';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editing: false,
+      editing: true,
       editingExisting: false,
       library: [],
-      activeDictionary: [],
+      title: 'Noname',
+      input: [
+        {
+          domain: '',
+          range: '',
+        },
+        {
+          domain: '',
+          range: '',
+        },
+      ],
     };
   }
 
@@ -24,11 +35,100 @@ class App extends Component {
   }
 
   handleBoardChange = () => {
-    this.setState(states => ({
-      editing: !states.editing,
-    }));
+    const { editing } = this.state;
+    const newState = {
+      editing: !editing,
+    };
+    if (editing) {
+      newState.input = [
+        {
+          domain: '',
+          range: '',
+        },
+        {
+          domain: '',
+          range: '',
+        },
+      ];
+      newState.title = 'Noname';
+    }
+
+    this.setState(newState);
   }
 
+  handlePairUpdate = (index, key, value) => {
+    const { input } = this.state;
+    input[index][key] = value;
+
+    // delete cells if they were emptied
+    const { domain, range } = input[index];
+    if (!domain && !range) {
+      input.splice(index, 1);
+    }
+    // add empty if the last was used
+    const lastItem = input[input.length - 1];
+    if (lastItem.domain !== '' || lastItem.range !== '') {
+      input.push({
+        domain: '',
+        range: '',
+      });
+    }
+    this.setState({
+      input,
+    });
+  }
+
+  handlePairDelete = (index) => {
+    const { input } = this.state;
+    input.splice(index, 1);
+    // don't delete entirely
+    if (input.length === 0) {
+      input.push({
+        domain: '',
+        range: '',
+      });
+    }
+    this.setState({
+      input,
+    });
+  }
+
+  handleTitleChange = (e) => {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  handleSaveClick = () => {
+    const { input, title } = this.state;
+
+
+    // validate
+    const validatedDict = Validator.validateAll(input);
+
+
+    // validation has not passed
+    if (validatedDict.errors) {
+      validatedDict.pairs.push({
+        domain: '',
+        range: '',
+      });
+      this.setState({
+        input: validatedDict.pairs,
+      });
+    // succesfully validated
+    } else {
+      const newDict = {
+        title,
+        date: String((new Date()).toLocaleDateString('en-GB', {
+          minute: 'numeric', hour: 'numeric', month: 'numeric', year: 'numeric', day: 'numeric',
+        })),
+        content: validatedDict.newDict,
+      };
+      // save
+      this.handleSave(newDict);
+    }
+  }
 
   handleSave = (newDict) => {
     const { library, editingExisting } = this.state;
@@ -43,13 +143,24 @@ class App extends Component {
       editing: false,
       editingExisting: false,
       activeDictionary: [],
+      title: 'Noname',
+      input: [
+        {
+          domain: '',
+          range: '',
+        },
+        {
+          domain: '',
+          range: '',
+        },
+      ],
     });
 
     // save to LS
     window.localStorage.setItem('library', JSON.stringify(library));
   }
 
-  handleDelete =(index) => {
+  handleDictionaryDelete =(index) => {
     const { library } = this.state;
     library.splice(index, 1);
     this.setState({
@@ -70,11 +181,8 @@ class App extends Component {
       range: '',
     });
     this.setState({
-      activeDictionary:
-        {
-          title,
-          pairs,
-        },
+      title,
+      input: pairs,
       editing: true,
       editingExisting: index,
     });
@@ -82,8 +190,8 @@ class App extends Component {
 
   render() {
     const {
- editing, library, activeDictionary, reset 
-} = this.state;
+      editing, library, title, input,
+    } = this.state;
     return (
       <div className="App">
         <Title />
@@ -91,10 +199,17 @@ class App extends Component {
         <DictionaryList
           className={editing ? 'hide' : ''}
           library={library}
-          handleDelete={this.handleDelete}
+          handleDelete={this.handleDictionaryDelete}
           handleEdit={this.handleEdit}
         />
-        <Editor className={!editing ? 'hide' : ''} handleSave={this.handleSave} activeDictionary={activeDictionary} />
+        <Editor
+          className={!editing ? 'hide' : ''}
+          handleSaveClick={this.handleSaveClick}
+          activeDictionary={{ title, input }}
+          handlePairUpdate={this.handlePairUpdate}
+          handlePairDelete={this.handlePairDelete}
+          handleTitleChange={this.handleTitleChange}
+        />
         <Footer />
       </div>
     );
